288efbf764ac686f52b5737244b4046184108be0
Faranosh  Amini <faranosh.amini4@gmail.com>
2022-07-05 13:37:00 +0430

    Solved Linters errors

--- js/index.js
+++ js/index.js
@@ -1,39 +1,39 @@
 const books = [
   {
-    title: "abc",
-    author: "Aijaz",
+    title: 'abc',
+    author: 'Aijaz',
   },
   {
-    title: "def",
-    author: "Riyaz",
+    title: 'def',
+    author: 'Riyaz',
   },
   {
-    title: "ghi",
-    author: "Pervez",
+    title: 'ghi',
+    author: 'Pervez',
   },
 ];
 
 const showBookList = (book) => {
-  const booksDiv = document.getElementById("list");
-  const div = document.createElement("div");
-  div.setAttribute("class", "book-info");
-  div.setAttribute("id", "book-info");
+  const booksDiv = document.getElementById('list');
+  const div = document.createElement('div');
+  div.setAttribute('class', 'book-info');
+  div.setAttribute('id', 'book-info');
   booksDiv.append(div);
-  const title = document.createElement("h5");
-  title.setAttribute("class", "title");
+  const title = document.createElement('h5');
+  title.setAttribute('class', 'title');
   title.innerHTML = book.title;
   div.append(title);
-  const author = document.createElement("p");
-  author.setAttribute("class", "author");
+  const author = document.createElement('p');
+  author.setAttribute('class', 'author');
   author.innerHTML = book.author;
   div.append(author);
-  const button = document.createElement("button");
-  button.setAttribute("class", "removebook");
-  button.addEventListener("click", (e) => {
-    const sectionName = document.getElementById("book-info");
+  const button = document.createElement('button');
+  button.setAttribute('class', 'removebook');
+  button.addEventListener('click', () => {
+    const sectionName = document.getElementById('book-info');
     sectionName.remove();
   });
-  button.innerHTML = "Remove";
+  button.innerHTML = 'Remove';
   div.append(button);
 };
 
