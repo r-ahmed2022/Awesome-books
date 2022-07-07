915dfecb4ad1aeba3e702dd3abe23704a0a3fc12
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-07-07 19:25:05 +0530

    updated index.js with localstorage feature

--- js/index.js
+++ js/index.js
@@ -1,86 +1,44 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable indent */
-const storedList = JSON.parse(localStorage.getItem('books') || []);
-function validateFields() {
-if (document.getElementById('title').value === '' || document.getElementById('author').value === '') {
-    return false;
-}
-    return true;
-  }
-
 class Books {
-    constructor(id, title, author) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
+    booklist = [];
+    addBooks () {
+      const name = document.getElementById('title').value;
+      const author = document.getElementById('author').value;
+      this.booklist.push({ title: name, author: author });
+      localStorage.setItem('books', JSON.stringify(this.booklist));
+      location.reload();
+    };
+  }
+  let book = new Books();
+  
+  function showBookList() {
+    let html = '<div>';
+    html += '<table class="center">'
+    const storedBook = JSON.parse(localStorage.getItem('books'));
+    for (let i = 0; i < storedBook.length; i += 1) {
+      book.booklist.push({ title: storedBook[i].title, author: storedBook[i].author });
+      html += '<tr>'
+      html += '<td> "' + book.booklist[i].title + '" by ' + book.booklist[i].author + '</td>';
+      html += '<td class="test"><button onclick="deleteBook(' + i + ')"> Remove </button> </td>';
+      html += '</tr>'
+      document.getElementById('list').innerHTML = html;
     }
-
-   booklist = [];
-
-   addBook() {
-    if (validateFields()) {
-         const bookname = document.getElementById('title').value;
-         const authorname = document.getElementById('author').value;
-         // eslint-disable-next-line max-len
-         this.booklist.push({ title: bookname, author: authorname });
-         storedList.setItem('books', JSON.stringify(this.booklist));
-                   }
-       document.getElementById('title').value = '';
-       document.getElementById('author').value = '';
-       }
-}
-const book = new Books();
-
-document.getElementById('addbook').addEventListener('click', () => {
-    book.addBook();
- });
-
-// eslint-disable-next-line no-unused-vars
-// eslint-disable-next-line consistent-return
-function remove(e) {
-    const newList = book.booklist.filter((element) => e.target.parentElement !== element);
-    localStorage.setItem('books', JSON.stringify(newList));
-    e.target.parentElement.remove();
+    html += '</table>'
   }
 
-function showBookList() {
-    if (storedList !== null) {
-    storedList.forEach((element) => {
-      const booksDiv = document.getElementById('list');
-      const div = document.createElement('div');
-      div.setAttribute('class', 'book-info');
-      div.setAttribute('id', 'book-info');
-      booksDiv.append(div);
-      const title = document.createElement('h5');
-      title.setAttribute('class', 'title');
-      title.innerHTML = element.title;
-      div.append(title);
-      const author = document.createElement('p');
-      author.setAttribute('class', 'author');
-      author.innerHTML = element.author;
-      div.append(author);
-      const button = document.createElement('button');
-      button.setAttribute('class', 'removebook');
-      button.addEventListener('click', (e) => {
-             remove(e);
-      });
-      button.innerHTML = 'Remove';
-      div.append(button);
-    });
-  } else {
-    alert('Empty list, please add books');
-  }
-}
+  const button = document.getElementById('addbook');
 
-document.addEventListener('DOMContentLoaded', () => {
-        showBookList();
+  button.addEventListener('click', () => {
+    book.addBooks();
+  });
+  
+  function deleteBook(i) {
+    book.booklist.splice(i, 1);
+    localStorage.setItem('books', JSON.stringify(book.booklist));
+    location.reload();
+  }
+  
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    showBookList();
 });
-
-const today = new Date();
-const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-
-const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-
-const dateTime = `${date} ${time}`;
-
-document.querySelector('.date-text').innerHTML = dateTime;
+  
